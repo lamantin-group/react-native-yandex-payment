b506145f03ed2290f6a1f60e32d99d6a15d05d7c
whalemare <nanostet@gmail.com>
2019-08-29 21:12:30 +0700

    YAHHOOOOO!!! ADD WORKING SAMPLE

---
+++ metro.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path')
+const blacklist = require('metro-config/src/defaults/blacklist')
+const escape = require('escape-string-regexp')
+const pak = require('./package.json')
+
+const peerDependencies = Object.keys(pak.peerDependencies)
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+
+  projectRoot: path.resolve(__dirname, 'example'),
+  watchFolders: [__dirname],
+
+  resolver: {
+    blacklistRE: blacklist([
+      new RegExp(`^${escape(path.resolve(__dirname, 'node_modules'))}\\/.*$`),
+    ]),
+    providesModuleNodeModules: ['@babel/runtime', ...peerDependencies],
+    extraNodeModules: {
+      'react-native-web-image': __dirname,
+    },
+  },
+}
