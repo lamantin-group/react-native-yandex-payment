e5d22965bb8dfc0b9929d039679e0e944d96ce4a
whalemare <nanostet@gmail.com>
2019-09-04 09:03:44 +0700

    Build ios sample without framework pod

--- ios/MyFramework.podspec
+++ ios/MyFramework.podspec
@@ -63,10 +63,10 @@ Pod::Spec.new do |spec|
   #
 
   # spec.platform     = :ios
-  # spec.platform     = :ios, "5.0"
-
+  spec.platform     = :ios, "9.0"
+  
   #  When using multiple platforms
-  # spec.ios.deployment_target = "5.0"
+  spec.ios.deployment_target = "9.0"
   # spec.osx.deployment_target = "10.7"
   # spec.watchos.deployment_target = "2.0"
   # spec.tvos.deployment_target = "9.0"
@@ -89,7 +89,7 @@ Pod::Spec.new do |spec|
   #  Not including the public_header_files will make all headers public.
   #
 
-  spec.source_files  = "Sources", "*.{h,m,swift}"
+  spec.source_files  = "MyFramework"
   spec.dependency 'React'
   spec.dependency 'YandexCheckoutPayments'
   # spec.public_header_files = "Classes/**/*.h"
--- ios/MyFramework/MyFramework-Bridging-Header.h
+++ ios/MyFramework/MyFramework-Bridging-Header.h
@@ -2,12 +2,30 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+//#import <Foundation/Foundation.h>
+
+//#include <UIKit/UIKit.h>
 // required for all types of bridge
-#import "React/RCTBridgeModule.h"
+//#import "React/RCTBridgeModule.h"
 
 // required only for UI Views
-#import "React/RCTViewManager.h"
+//#import "React/RCTViewManager.h"
+
 // required only for Event Emitters
-#import "React/RCTEventEmitter.h"
+// #import "React/RCTEventEmitter.h"
+
 // required for calling methods on ViewManagers
-#import "React/RCTUIManager.h"
+// #import "React/RCTUIManager.h"
+
+#import <Foundation/Foundation.h>
+
+#import "React/RCTBridgeModule.h"
+#import "React/RCTViewManager.h"
+
+@interface RCT_EXTERN_MODULE(YandexPayment, RCTViewManager)
+
+RCT_EXTERN_METHOD(attach:
+                  (NSDictionary *) map
+                  callbacker:(RCTResponseSenderBlock) callback
+                  )
+@end
